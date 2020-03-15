8bcf315a8a7b2be0613f06720942b6fd971e9d12
dyzcs <deng.kinger@gmail.com>
2020-03-15 12:50:06 +0800

    gateway

---
+++ cloud-gateway-gateway9527/src/main/java/com/deng/mscloud/GateWayMain9527.java
@@ -0,0 +1,16 @@
+package com.deng.mscloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Created by Administrator on 2020/3/15.
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class, args);
+    }
+}
---
+++ cloud-gateway-gateway9527/src/main/java/com/deng/mscloud/config/GateWayConfig.java
@@ -0,0 +1,28 @@
+package com.deng.mscloud.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Administrator on 2020/3/15.
+ */
+@Configuration
+public class GateWayConfig {
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+        routes.route("path_route_deng",
+                r -> r.path("/guonei").uri("http://news.baidu.com/guonei")).build();
+        return routes.build();
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator2(RouteLocatorBuilder routeLocatorBuilder) {
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+        routes.route("path_route_deng2",
+                r -> r.path("/guoji").uri("http://news.baidu.com/guoji")).build();
+        return routes.build();
+    }
+}
